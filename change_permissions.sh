1518cfd93383a1798a0c0cff63d0b5a1eade21ca
Kathy Snider <sniderk@ohsu.edu>
2020-03-23 12:34:04 -0700

    Fix paths.

--- change_permissions.sh
+++ change_permissions.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-chmod -R 775 app
+chmod -R 775 /mnt/max/shared/code/internal/utilities/dcan-stack_dockerfiles/dcan-infant-pipeline
+chmod -R 775 /mnt/max/shared/code/internal/utilities/dcan-stack_dockerfiles/infant-abcd-bids-pipeline/app
