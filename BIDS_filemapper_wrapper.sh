53ef3e07888a56b3d363630f33aca64416df472f
Kathy Snider <sniderk@ohsu.edu>
2021-04-06 18:55:11 -0700

    Fix applicaton name.

--- BIDS_filemapper_wrapper.sh
+++ BIDS_filemapper_wrapper.sh
@@ -27,7 +27,7 @@ if [ -d ${scripts_dir} ] && [ -e ${scripts_dir}/SubmitterEnv.sh ] ; then
     source ${scripts_dir}/SubmitterEnv.sh
 fi
 
-fmscript=${FILEMAPPERDIR}/file-mapper
+fmscript=${FILEMAPPERDIR}/file-mapper.py
 CMD="${fmscript} ${TEMPLATE_JSON} -a copy -o -s -sp ${FILES} -dp ${ROOT} -t SUBJECT=${SUB},SESSION=${SES},PIPELINE=${PIPE}"
 set -x
 eval ${CMD}
