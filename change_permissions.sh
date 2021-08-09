acecc94d5edd5181f0ab7d4fae9692e6f0d9ff4b
Kathy Snider <sniderk@ohsu.edu>
2021-08-09 10:25:38 -0700

    Local scripts.

--- change_permissions.sh
+++ change_permissions.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 chmod -R 775 /mnt/max/shared/code/internal/utilities/dcan-stack_dockerfiles/dcan-infant-pipeline
-chmod -R 775 /mnt/max/shared/code/internal/utilities/dcan-stack_dockerfiles/infant-abcd-bids-pipeline/app
+chmod -R 775 /mnt/max/shared/code/internal/utilities/dcan-stack_dockerfiles/infant-abcd-bids-pipeline
